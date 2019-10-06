63996e105ecb3041b7062e90de9785d7074abe8b
John Jordan <jordanjohnp@yahoo.com>
2019-10-06 22:52:08 +0000

    hookup pause

--- src/app/views.py
+++ src/app/views.py
@@ -13,6 +13,8 @@ def shutdown_server():
     if func is None:
         raise RuntimeError('Not running with the Werkzeug Server')
     func()
+    # JJ
+    kill_explore()
 
 @app.route('/shutdown')
 def shutdown():
@@ -56,21 +58,26 @@ def key_released():
 @app.route('/loadData/')
 def loadData():
     print("Page Loaded")
-    data = {
+
+    # JJ
+    app.data = {
         'battery_percentage': 88,
         'robot_name': "turty3_pro",
         'past_runs': [
 
         ]
-    }
+    }    
     for i in range(6):
-        data['past_runs'].append({
+        app.data['past_runs'].append({
             "name": "run" + str(i),
             "path": "/path/to/run_" + str(i),
             "date": "2019-09-0" + str(i),
             "length": "8:" + str(i) + str(i),
             })
+        '''
     return jsonify(data)
+    '''
+    return jsonify(app.data)
 
 
 @app.route('/startNavigation/')
@@ -78,21 +85,34 @@ def start_nav():
     print("START NAV")
 
     # JJ
-    devnull = open('/dev/null', 'w')
-    #current_app.config['explore_process'] = subprocess.Popen(["./mini_turty_explore.sh"], stdout=devnull, shell=False)
-    current_app.config['explore_process'] = subprocess.Popen(["/home/ubuntu/mini_turty_mapping.sh"], stdout=devnull, shell=False)
+    if current_app.config['controller'].get_paused() == True:
+        print "Robot controller was PAUSED, unpausing now"
+        current_app.config['controller'].set_paused(False)
+    else:
+        print "Robot controller was NOT PAUSED, starting explore_process now"
+        devnull = open('/dev/null', 'w')
+        #current_app.config['explore_process'] = subprocess.Popen(["./mini_turty_explore.sh"], stdout=devnull, shell=False)
+        current_app.config['explore_process'] = subprocess.Popen(["./mini_turty_explore.sh"], shell=False)
 
     return jsonify({})
     
 @app.route('/pauseNavigation/')
 def pause_nav():
     print("PAUSE NAV")
+
+    # JJ
+    current_app.config['controller'].set_paused(True)
+
     return jsonify({})
 
 @app.route('/saveNavigation/')
 def save_nav():
     location = request.args.get("location")
     print("SAVE NAV", location)
+    
+    # JJ 
+    current_app.config['controller'].set_paused(False)
+    kill_explore()
 
     return jsonify({})
     
@@ -101,11 +121,8 @@ def discard_nav():
     print("DISCARD NAV")
     
     # JJ 
-    pid = current_app.config['explore_process'].pid
-    os.kill(pid, signal.SIGINT)
-
-    if not current_app.config['explore_process'].poll():
-        print "Process correctly halted"
+    current_app.config['controller'].set_paused(False)
+    kill_explore()
 
     return jsonify({})
 
@@ -121,6 +138,12 @@ def latestMap():
     path = "static/img/map.jpg"
     return send_file(path)
 
+# JJ
+def kill_explore ():
+    pid = current_app.config['explore_process'].pid
+    os.kill(pid, signal.SIGINT)
+    if not current_app.config['explore_process'].poll():
+        print "Process correctly halted"
 
 if __name__ == '__main__':
     raise Exception('test')
